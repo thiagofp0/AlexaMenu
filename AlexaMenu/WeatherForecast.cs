8fe56543373e54b30f236276486d55c49a316ad2
Thiago <thiagofp707@gmail.com>
2022-02-26 00:07:08 -0300

    Initial Commit

---
+++ AlexaMenu/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace AlexaMenu;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}

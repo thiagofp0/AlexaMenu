35971547ec1ff5c185d39edcbaebbddf7cffc30a
Thiago <thiagofp707@gmail.com>
2023-10-29 00:41:23 -0300

    Corrigindo erro mapping

--- v2/AlexaMenu.Infrastructure/Mapping/MapProfile.cs
+++ v2/AlexaMenu.Infrastructure/Mapping/MapProfile.cs
@@ -9,15 +9,24 @@ namespace AlexaMenu.Infrastructure.Mapping
 	{
 		public MapProfile()
 		{
-            CreateMap<MenuDocumentModel, Menu>().ForMember(destination => destination.Id, cfg => cfg.Ignore());
+            CreateMap<MenuDocumentModel, Menu>().ForMember(destination => destination.Id, cfg => cfg.Ignore())
+                .ForMember(destination => destination.Date, cfg => cfg.MapFrom(source => DateTime.Parse(source.Date)))
+                .ForMember(destination => destination.Meals, cfg => cfg.MapFrom(source => MapMealDocumentModelToMeal(source.Meals)));
+
+            CreateMap<MealDocumentModel, Meal>().ForMember(destination => destination.Id, cfg => cfg.Ignore())
+                .ForMember(destination => destination.StartTime, cfg => cfg.MapFrom(source => source.StartTime))
+                .ForMember(destination => destination.EndTime, cfg => cfg.MapFrom(source => source.EndTime))
+                .ForMember(destination => destination.Dishes, cfg => cfg.MapFrom(source => MapDishDocumentObjectToDish(source.Dishes)));
+
+            CreateMap<DishDocumentModel, Dish>().ForMember(destination => destination.Id, cfg => cfg.Ignore());
 
             CreateMap<MenuSaveCommandObject, MenuDocumentModel>()
                 .ForMember(destination => destination.Date, cfg => cfg.MapFrom(source => source.Date.ToShortDateString()))
-                .ForMember(destination => destination.Meals, cfg => cfg.MapFrom(source => MapMeals(source.Meals)))
+                .ForMember(destination => destination.Meals, cfg => cfg.MapFrom(source => MapMealApiModelMealsToDocumentModel(source.Meals)))
                 .ForMember(destination => destination.Id, cfg => cfg.Ignore());
         }
 
-        List<MealDocumentModel> MapMeals(List<Meal> meals)
+        static List<MealDocumentModel> MapMealApiModelMealsToDocumentModel(List<Meal> meals)
         {
             return meals.Select(item =>
             new MealDocumentModel
@@ -26,11 +35,11 @@ namespace AlexaMenu.Infrastructure.Mapping
                 Restaurant = item.Restaurant,
                 StartTime = item.StartTime,
                 EndTime = item.EndTime,
-                Dishes = MapDishes(item.Dishes)
+                Dishes = MapDishApiModelDishToDocumentModel(item.Dishes)
             }).ToList();
         }
 
-        List<DishDocumentModel> MapDishes(List<Dish> dishes)
+        static List<DishDocumentModel> MapDishApiModelDishToDocumentModel(List<Dish> dishes)
         {
             return dishes.Select(item =>
             new DishDocumentModel
@@ -39,6 +48,18 @@ namespace AlexaMenu.Infrastructure.Mapping
                 Category = item.Category,
                 Note = item.Note
             }).ToList();
+        }
+
+        static List<Meal> MapMealDocumentModelToMeal(List<MealDocumentModel> meals)
+        {
+            return meals.Select(item =>
+            new Meal(item.Name, item.Restaurant, item.StartTime, item.EndTime, MapDishDocumentObjectToDish(item.Dishes))).ToList();
+        }
+
+        static List<Dish> MapDishDocumentObjectToDish(List<DishDocumentModel> dishes)
+        {
+            return dishes.Select(item =>
+            new Dish(item.Name, item.Category, item.Note)).ToList();
         }
     }
 }
